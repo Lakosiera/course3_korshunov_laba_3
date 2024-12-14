ea490248616f641dcaa524570965efd9ca0ba001
Lakosiera <lakosiera@mail.ru>
2024-12-14 21:22:43 +0500

    шаблоны

--- server/laba_3/views.py
+++ server/laba_3/views.py
@@ -3,43 +3,76 @@ from django.http import HttpResponse
 
 from .models import WordQuiz
 
+# константа с именем cookie
 COOKIE_NAME = "quiz_last"
 
 
 # пример простейшей вьющки
 def hello_world(request):
+    # простейший вывод html страницы
     return HttpResponse("Hello, world!")
 
 
 # функция для рендера вьюшки корневой страницы
 def index(request):
+    # запрашиваем из базы все поля таблицы WordQuiz
     word_list = WordQuiz.objects.all()
-    context = {"word_list": word_list}
-    # ренедр шаблона 'templates/index.html'
-    return render(request, "index.html", context)
+    # получаем id последнего переведенного слова из cookie брайзера
+    last_index = get_cookie_int(request, COOKIE_NAME)
+    # создаем словарь контекса чтобы передать переменные в шаблон
+    context = {
+        # все запрошенные слова
+        "word_list": word_list,
+        # последний индекс
+        "last_index": last_index,
+    }
+    # ренедр вьюшки в html страницу
+    return render(
+        # запрос самой вьюшки
+        request, 
+        # имя шаблона
+        template_name="index.html",
+        # контекст с переменными
+        context=context,
+    )
 
 
+# функция для рендера вьюшки перевода слова
 def word_quiz(request, word_id):
-    cookie_value = request.COOKIES.get(COOKIE_NAME, "default_value")
+    # запросить из бызы обьект с идентификатором word_id, в случае нейдачи вывести старницу 404
     word_quiz = get_object_or_404(WordQuiz, pk=word_id)
-    context = {"word_quiz": word_quiz, "cookie_value": cookie_value}
+    # получаем id последнего переведенного слова из cookie брайзера
+    last_index = get_cookie_int(request, COOKIE_NAME)
+    # создаем словарь контекса чтобы передать переменные в шаблон
+    context = {
+        # данные слова из таблицы 
+        "word_quiz": word_quiz,
+        # последний индекс
+        "last_index": last_index,
+    }
+    # ренедр вьюшки в html страницу
     return render(request, "word_quiz.html", context)
 
 
+# функция для валидации формы
 def check(request, word_id):
-    response = HttpResponse(f"id {word_id} {type(word_id).__name__}")
-    last_index = 0
-    try:
-        cookie_string = request.COOKIES.get(COOKIE_NAME, "0")
-        last_index = int(cookie_string)
-    except ValueError:
-        last_index = 0
-    if word_id > last_index:
-        response.set_cookie(COOKIE_NAME, word_id)
+    last_index = get_cookie_int(request, COOKIE_NAME)
+    response = HttpResponse(f"id= {word_id} last= {last_index}")
+
+    if word_id >= last_index:
+        response.set_cookie(COOKIE_NAME, word_id + 1)
     return response
 
 
-def get_cookie_view(request):
-    # TODO
-    cookie_value = request.COOKIES.get(COOKIE_NAME, "default_value")
-    return HttpResponse(f"The value of my_cookie is {cookie_value}")
+def reset(request):
+    response = HttpResponse("Cookie удален!")
+    response.delete_cookie(COOKIE_NAME)
+    return response
+
+
+def get_cookie_int(request, key, default=0):
+    try:
+        cookie_string = request.COOKIES.get(key, default)
+        return int(cookie_string)
+    except ValueError:
+        return default

79604f2ef89a13ac441b2378bc1672e6c72a36a2
Lakosiera <lakosiera@mail.ru>
2024-12-14 17:32:39 +0500

    админка

--- server/laba_3/admin.py
+++ server/laba_3/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import WordQuiz
 
-# регистрируем классы моделей бля работы с ними в админки
 
-admin.site.register(WordQuiz)
+# класс обвертка над 'WordQuiz' для работы в админке
+class WordQuizAdmin(admin.ModelAdmin):
+    # отображаем поля обьекта в админке
+    list_display = ('id', 'word', 'translation')
+
+
+# регистрируем классы моделей бля работы с ними в админке
+admin.site.register(WordQuiz, WordQuizAdmin)
